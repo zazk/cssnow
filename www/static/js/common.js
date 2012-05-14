f9b658ae403e8189276420b41a57f902c9938b7c
Enrique Juan de Dios <juandedioz@gmail.com>
2012-05-14 11:30:18 -0500

    Add backgroun fx

--- www/static/js/common.js
+++ www/static/js/common.js
@@ -1,1 +1,7 @@
 // JavaScript Document
+$(function(){
+  var j = 0;
+  var i = 0;
+  setInterval(function(){$('body').css('background-position', i++ )},50)
+  setInterval(function(){$('#wrap').css('background-position', j-- )},50)
+});
